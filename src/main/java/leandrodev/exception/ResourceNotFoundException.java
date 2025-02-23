ed6bdfeb9c1e196a0fc000c52e5eae95d7187bed
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-02-23 19:27:59 -0300

    personalizada para recursos não encontrados
    Criar exceção personalizada para recursos não encontrados

---
+++ src/main/java/leandrodev/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package leandrodev.exception;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
