08c9fce0abc8e4eada4bdf6e2bd406fae9030d07
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-02-23 19:31:35 -0300

    Voto com relacionamento com User e Candidate
     Criar modelo de Voto com relacionamento com User e Candidate

---
+++ src/main/java/leandrodev/model/Vote.java
@@ -0,0 +1,45 @@
+package leandrodev.model;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "votes")
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "candidate_id", nullable = false)
+    private Candidate candidate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Candidate getCandidate() {
+        return candidate;
+    }
+
+    public void setCandidate(Candidate candidate) {
+        this.candidate = candidate;
+    }
+}
