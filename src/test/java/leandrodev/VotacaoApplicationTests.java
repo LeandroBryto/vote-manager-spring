326e9e29a1ac50fe3c6db8beb4688b0da30d8dac
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-02-23 19:32:33 -0300

    Create VotacaoApplicationTests.java

---
+++ src/test/java/leandrodev/VotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package leandrodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
